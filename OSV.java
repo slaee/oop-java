4c467aa7af265df5ad79a6a84cabdc635b89211f
Sly Kint A. Bacalso <64179667+Son0fAnton@users.noreply.github.com>
2021-11-02 21:11:15 +0800

    update

--- OSV.java
+++ OSV.java
@@ -1,13 +1,28 @@
+/**
+ * @author Sly Kint Bacalso
+ * @version JDK 11
+ * 
+ ** Hey there enjoy using my modules you can copy it to your
+ ** Java OOP lesson and always credit me. Thank you!
+ ** HAPPY HACKING 
+ */
+
 import java.util.*;
 import osvclass.OsvMath;
 
 public class OSV{
+    
+    /** 
+     * @param args
+     */
     public static void main(String [] args) {
+
         Scanner num = new Scanner(System.in);
         int numerator = num.nextInt();
         int denominator = num.nextInt();
-
         OsvMath.getMixedFraction(numerator, denominator);
+
+        // always close scanner after using it
         num.close();
     }
 }
--- osvclass/OsvMath.java
+++ osvclass/OsvMath.java
@@ -5,6 +5,15 @@ public class OsvMath {
 
     }
 
+    /**
+     * Calcualtes the the fraction numerator and 
+     * fraction denominator using Euclidean Algorithm
+     * 
+     * @param fn
+     * @param fd
+     * @return a
+     * @see https://en.wikipedia.org/wiki/Euclidean_algorithm
+     */
     public static int euclideanGcd(int fn, int fd){
         int a = fn, b = fd;
         while(b != 0){
@@ -15,12 +24,17 @@ public class OsvMath {
         return a;
     }
 
+    /**
+     * This method wi
+     * @param numerator
+     * @param denominator
+     */
     public static void getMixedFraction(int numerator, int denominator){
         int wholeNumber = numerator / denominator;
         int fn = numerator % denominator;
         int fd = denominator;
         int gcd = euclideanGcd(fn, fd);
-        
+
         fn /= gcd;
         fd /= gcd;
         System.out.println(wholeNumber+" "+fn+"/"+fd);
