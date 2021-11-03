0c2b76bfd11ab178ce8ee820fc63b233508203ad
Sly Kint A. Bacalso <64179667+Son0fAnton@users.noreply.github.com>
2021-11-03 20:59:50 +0800

    added light object

--- OSV.java
+++ OSV.java
@@ -4,10 +4,12 @@
  * 
  ** Hey there enjoy using my modules you can copy it to your
  ** Java OOP lesson and always credit me. Thank you!
- ** HAPPY HACKING 
+ ** HAPPY HACKING :))
  */
 
 import java.util.*;
+
+import osvclass.OsvLight;
 import osvclass.OsvMath;
 
 public class OSV{
@@ -17,12 +19,18 @@ public class OSV{
      */
     public static void main(String [] args) {
 
-        Scanner num = new Scanner(System.in);
-        int numerator = num.nextInt();
-        int denominator = num.nextInt();
-        OsvMath.getMixedFraction(numerator, denominator);
+        // Scanner num = new Scanner(System.in);
+        // int numerator = num.nextInt();
+        // int denominator = num.nextInt();
+        // OsvMath.getMixedFraction(numerator, denominator);
 
-        // always close scanner after using it
-        num.close();
+        // // always close scanner after using it
+        // num.close();
+
+        // visualizing light interfaces
+        OsvLight light = new OsvLight();
+        light.on();
+        light.off();
+        light.status();
     }
 }
